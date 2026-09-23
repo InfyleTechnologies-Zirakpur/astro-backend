79c998cde62829f8916602a875029c57b7594477
chirayu-seth <sethchirayu@gmail.com>
2026-09-23 13:51:40 +0530

    done

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,13 +1,25 @@
-const admin = require("firebase-admin");
+const {
+  initializeApp,
+  cert,
+  getApps,
+} = require("firebase-admin/app");
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
+const {
+  getMessaging,
+} = require("firebase-admin/messaging");
+
+if (getApps().length === 0) {
+  initializeApp({
+    credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     }),
   });
 }
 
-module.exports = admin;
+const messaging = getMessaging();
+
+module.exports = {
+  messaging,
+};
